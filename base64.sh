ea2c936d2dbb4a2efb19a8ea86d3643c481e03fc
SunnyH4ck3r <98934720+SunnyH4ck3r@users.noreply.github.com>
2022-02-27 22:11:26 -0500

    Add files via upload

---
+++ base64.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo 'Elige: \n 1.- Para encriptar. \n 2.- Para desencriptar'
+read var1
+if [ "$var1" -eq 1 ];
+	then
+	echo 'Introduce el valor a encriptar'
+	read var2
+	  echo 'Este es el resultado:' $var2 | base64
+	exit 1
+elif [ "$var1" -eq 2 ];
+	then
+	echo 'Introduce el valor a desencriptar'
+	read var2
+	  echo "$var2" | base64 -d
+	exit 1
+else
+	  echo "Parece que ha habido un error introduce 1 o 2."
+fi
